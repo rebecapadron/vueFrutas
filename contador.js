e995ae0885d2660e49c119df3f55bfc0d5cadacf
Alex <alex0195@gmail.com>
2021-12-13 19:15:19 -0300

    dos nuevos metodos

--- contador.js
+++ contador.js
@@ -1,5 +1,13 @@
 const numeros = [1,2,3,4,5,6,7,8]
 
-for (let i = 0; i < numeros.length; i++) {
-    console.log(numeros[i]);
-}
+// for (let i = 0; i < numeros.length; i++) {
+//     console.log(numeros[i]);
+// }
+
+// for(let num of numeros){
+//     console.log(num);
+// }
+
+numeros.forEach(function(numero){
+    console.log(numero);
+})
