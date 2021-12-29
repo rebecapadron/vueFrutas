3bf6fcc6c9b71d9cd19229bf7b0034e01391432c
Alexander Padron <alex0195@gmail.com>
2021-12-28 23:09:24 -0300

    commit 2

---
+++ 01.js
@@ -0,0 +1,28 @@
+const app= new Vue ({
+     el: '#app',
+     data: {
+         titulo: 'hola mundo con vue',
+         frutas: [
+            {nombre: 'Pera', cantidad: 10},
+            {nombre: 'Manzana', cantidad: 0},
+            {nombre: 'platano', cantidad: 11}
+     ], 
+     nuevaFruta: ''
+
+ },
+ methods:{
+    agregarFruta () {
+        const existe = this.frutas.filter(fruta => fruta.nombre == this.nuevaFruta)
+        if(existe.length == 0){
+         this.frutas.push({
+            nombre: this.nuevaFruta, 
+            cantidad: 0
+         })
+        }else{
+            console.log(`Ya existe una fruta con el nombre ${this.nuevaFruta}`)
+        }
+     }
+ }
+ 
+
+})
